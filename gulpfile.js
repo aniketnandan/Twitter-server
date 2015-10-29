92e5f9fb69ae259b4e14c3b2137fc79e3ff3abf4
aniket nandan <anandan@kreeti.com>
2015-10-29 12:56:15 +0530

    gulp with eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,50 @@
+'use strict';
 var gulp = require('gulp');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
+var source = require('vinyl-source-stream');//vinyl-source-stream convert the readable stream you get from browserify into a vinyl stream that is what gulp is expecting to get.
 var babelify = require('babelify');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var del= require('del');
 var watch = require('gulp-watch');
+var eslint = require('gulp-eslint');
+var watchify = require('watchify');
 
-gulp.task('build', function(){
-  browserify({
+gulp.task('build',function(){
+  buildFile();
+  console.log("Builded");
+});
+
+function buildFile(){
+  var b = browserify({
     entries: 'public/components/Router_route.jsx',
     debug: true
-  })
-    .transform(babelify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('public/javascripts'));
+  });
+  b = watchify(b);
+  b.on('update',function(){
+    lintFile(b);
+  });
+};
+
+function bundleShare(b){
+  b.transform(babelify())
+   .bundle()
+   .pipe(source('bundle.js'))
+   .pipe(gulp.dest('public/javascripts'));
+};
+
+gulp.task('lint',function(){
+  lintFile();
 });
+
+function lintFile(b){
+  return gulp.src(['public/components/*.jsx'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+  if(b){
+    bundleShare(b);
+  }
+}
 
 gulp.task('delete',function(){
   del(['public/javascripts/bundle.js']).then(function(){
@@ -24,8 +52,8 @@ gulp.task('delete',function(){
   });
 });
 
-gulp.task('watch',['delete', 'build'],function(){
-  var watcher = gulp.watch('public/components/Router_route.jsx', ['delete', 'build']);
+gulp.task('watch',function(){
+  var watcher = gulp.watch('public/components/*.jsx', ['delete', 'build']);
   watcher.on('error', function() {
     console.log('waiting for changes')
   })
